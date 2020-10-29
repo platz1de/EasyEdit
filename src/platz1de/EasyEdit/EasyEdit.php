0db2a23ff6464af31a9e8545a2df3022fa5a4f45
platz1de <51201131+platz1de@users.noreply.github.com>
2020-10-29 22:18:16 +0100

    Start everything on Enable

--- src/platz1de/EasyEdit/EasyEdit.php
+++ src/platz1de/EasyEdit/EasyEdit.php
@@ -2,9 +2,45 @@
 
 namespace platz1de\EasyEdit;
 
+use platz1de\EasyEdit\worker\EditWorker;
+use platz1de\EasyEdit\worker\WorkerAdapter;
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
 
 class EasyEdit extends PluginBase
 {
+	/**
+	 * @var EasyEdit
+	 */
+	private static $instance;
+	/**
+	 * @var EditWorker
+	 */
+	private static $worker;
 
+	public function onEnable()
+	{
+		self::$instance = $this;
+
+		self::$worker = new EditWorker(Server::getInstance()->getLogger());
+		self::$worker->start();
+
+		$this->getScheduler()->scheduleRepeatingTask(new WorkerAdapter(), 1);
+	}
+
+	/**
+	 * @return EasyEdit
+	 */
+	public static function getInstance()
+	{
+		return self::$instance;
+	}
+
+	/**
+	 * @return EditWorker
+	 */
+	public static function getWorker()
+	{
+		return self::$worker;
+	}
 }
