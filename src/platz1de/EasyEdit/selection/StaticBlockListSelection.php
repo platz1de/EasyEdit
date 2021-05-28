bf0cbde70de9f96d033eedc15c7a6e8fe4686cf1
platz1de <51201131+platz1de@users.noreply.github.com>
2021-05-28 19:50:19 +0200

    Implement static blocklist (undo & redo) splitting

--- src/platz1de/EasyEdit/selection/StaticBlockListSelection.php
+++ src/platz1de/EasyEdit/selection/StaticBlockListSelection.php
@@ -3,8 +3,10 @@
 namespace platz1de\EasyEdit\selection;
 
 use Closure;
+use pocketmine\level\Level;
 use pocketmine\level\Position;
 use pocketmine\math\Vector3;
+use UnexpectedValueException;
 
 class StaticBlockListSelection extends BlockListSelection
 {
@@ -24,5 +26,34 @@ class StaticBlockListSelection extends BlockListSelection
 	public function useOnBlocks(Vector3 $place, Closure $closure): void
 	{
 		parent::useOnBlocks(new Vector3(), $closure);
+	}
+
+	/**
+	 * splits into 3x3 Chunk pieces
+	 * @return array
+	 */
+	public function split(): array
+	{
+		if ($this->piece) {
+			throw new UnexpectedValueException("Pieces are not split able");
+		}
+
+		$level = $this->getLevel();
+		if ($level instanceof Level) {
+			$level = $level->getFolderName();
+		}
+		$pieces = [];
+		for ($x = ($this->pos1->getX()) >> 4; $x <= ($this->pos2->getX()) >> 4; $x += 3) {
+			for ($z = ($this->pos1->getZ()) >> 4; $z <= ($this->pos2->getZ()) >> 4; $z += 3) {
+				$piece = new StaticBlockListSelection($this->getPlayer(), $level, new Vector3(max($x << 4, $this->pos1->getX()), max($this->pos1->getY(), 0), max($z << 4, $this->pos1->getZ())), min(48, $this->pos2->getX() - $x * 16 + 1, 48 + $x * 16 - $this->pos1->getX(), $this->pos2->getX() + 1 - $this->pos1->getX()), min($this->pos2->getY(), Level::Y_MASK) - $this->pos1->getY() + 1, min(48, $this->pos2->getZ() - $z * 16 + 1, 48 + $z * 16 - $this->pos1->getZ(), $this->pos2->getZ() + 1 - $this->pos1->getZ()), true);
+				for ($chunkX = 0; $chunkX < 3; $chunkX++) {
+					for ($chunkZ = 0; $chunkZ < 3; $chunkZ++) {
+						$piece->getManager()->setChunk($x + $chunkX, $z + $chunkZ, $this->getManager()->getChunk($x + $chunkX, $z + $chunkZ));
+					}
+				}
+				$pieces[] = $piece;
+			}
+		}
+		return $pieces;
 	}
 }
