ab0b19fdb2150d2559f7bad1785497a7063dac9e
platz1de <51201131+platz1de@users.noreply.github.com>
2021-07-07 15:59:22 +0200

    Offset chest relations

--- src/platz1de/EasyEdit/utils/TileUtils.php
+++ src/platz1de/EasyEdit/utils/TileUtils.php
@@ -5,6 +5,8 @@ namespace platz1de\EasyEdit\utils;
 use pocketmine\level\format\Chunk;
 use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\tile\Chest;
 use pocketmine\tile\Tile;
 
 class TileUtils
@@ -20,7 +22,14 @@ class TileUtils
 		$compoundTag->setInt(Tile::TAG_X, $compoundTag->getInt(Tile::TAG_X) + $offset->getFloorX());
 		$compoundTag->setInt(Tile::TAG_Y, $compoundTag->getInt(Tile::TAG_Y) + $offset->getFloorY());
 		$compoundTag->setInt(Tile::TAG_Z, $compoundTag->getInt(Tile::TAG_Z) + $offset->getFloorZ());
-		return $compoundTag;
+
+		//chest relation
+		if($compoundTag->hasTag(Chest::TAG_PAIRX, IntTag::class) && $compoundTag->hasTag(Chest::TAG_PAIRZ, IntTag::class)){
+			$compoundTag->setInt(Chest::TAG_PAIRX, $compoundTag->getInt(Chest::TAG_PAIRX) + $offset->getFloorX());
+			$compoundTag->setInt(Chest::TAG_PAIRZ, $compoundTag->getInt(Chest::TAG_PAIRZ) + $offset->getFloorZ());
+		}
+
+			return $compoundTag;
 	}
 
 	/**
