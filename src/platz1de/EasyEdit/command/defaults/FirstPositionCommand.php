cedc0d414c7f62860588b673ebd754f0ed8d0285
platz1de <51201131+platz1de@users.noreply.github.com>
2021-07-14 17:45:15 +0200

    Accept coordinates when selection positions

--- src/platz1de/EasyEdit/command/defaults/FirstPositionCommand.php
+++ src/platz1de/EasyEdit/command/defaults/FirstPositionCommand.php
@@ -4,13 +4,14 @@ namespace platz1de\EasyEdit\command\defaults;
 
 use platz1de\EasyEdit\command\EasyEditCommand;
 use platz1de\EasyEdit\selection\Cube;
+use pocketmine\math\Vector3;
 use pocketmine\Player;
 
 class FirstPositionCommand extends EasyEditCommand
 {
 	public function __construct()
 	{
-		parent::__construct("/pos1", "Set the first Position", "easyedit.position", "//pos1", ["/1"]);
+		parent::__construct("/pos1", "Set the first Position", "easyedit.position", "//pos1 [x] [y] [z]", ["/1"]);
 	}
 
 	/**
@@ -19,6 +20,10 @@ class FirstPositionCommand extends EasyEditCommand
 	 */
 	public function process(Player $player, array $args): void
 	{
-		Cube::selectPos1($player, $player->floor());
+		if(count($args) > 2){
+			Cube::selectPos1($player, new Vector3((int) $args[0], (int) $args[1], (int) $args[2]));
+		}else{
+			Cube::selectPos1($player, $player->floor());
+		}
 	}
 }
--- src/platz1de/EasyEdit/command/defaults/SecondPositionCommand.php
+++ src/platz1de/EasyEdit/command/defaults/SecondPositionCommand.php
@@ -4,13 +4,14 @@ namespace platz1de\EasyEdit\command\defaults;
 
 use platz1de\EasyEdit\command\EasyEditCommand;
 use platz1de\EasyEdit\selection\Cube;
+use pocketmine\math\Vector3;
 use pocketmine\Player;
 
 class SecondPositionCommand extends EasyEditCommand
 {
 	public function __construct()
 	{
-		parent::__construct("/pos2", "Set the second Position", "easyedit.position", "//pos2", ["/2"]);
+		parent::__construct("/pos2", "Set the second Position", "easyedit.position", "//pos2 [x] [y] [z]", ["/2"]);
 	}
 
 	/**
@@ -19,6 +20,10 @@ class SecondPositionCommand extends EasyEditCommand
 	 */
 	public function process(Player $player, array $args): void
 	{
-		Cube::selectPos2($player, $player->floor());
+		if(count($args) > 2){
+			Cube::selectPos2($player, new Vector3((int) $args[0], (int) $args[1], (int) $args[2]));
+		}else{
+			Cube::selectPos2($player, $player->floor());
+		}
 	}
 }
