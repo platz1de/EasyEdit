7814382495e52354b45cd995607a20e267b11c9b
platz1de <51201131+platz1de@users.noreply.github.com>
2021-07-27 20:19:26 +0200

    CompoundTag::hasTag was removed

--- src/platz1de/EasyEdit/utils/TileUtils.php
+++ src/platz1de/EasyEdit/utils/TileUtils.php
@@ -7,7 +7,6 @@ use pocketmine\block\tile\Tile;
 use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\IntTag;
-use pocketmine\world\format\Chunk;
 
 class TileUtils
 {
@@ -24,7 +23,7 @@ class TileUtils
 		$compoundTag->setInt(Tile::TAG_Z, $compoundTag->getInt(Tile::TAG_Z) + $offset->getFloorZ());
 
 		//chest relation
-		if ($compoundTag->hasTag(Chest::TAG_PAIRX, IntTag::class) && $compoundTag->hasTag(Chest::TAG_PAIRZ, IntTag::class)) {
+		if ($compoundTag->getTag(Chest::TAG_PAIRX) instanceof IntTag && $compoundTag->getTag(Chest::TAG_PAIRZ) instanceof IntTag) {
 			$compoundTag->setInt(Chest::TAG_PAIRX, $compoundTag->getInt(Chest::TAG_PAIRX) + $offset->getFloorX());
 			$compoundTag->setInt(Chest::TAG_PAIRZ, $compoundTag->getInt(Chest::TAG_PAIRZ) + $offset->getFloorZ());
 		}
