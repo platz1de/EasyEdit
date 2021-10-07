24914848b634a1effd058705843c3412b8bbacb0
platz1de <51201131+platz1de@users.noreply.github.com>
2021-10-07 22:58:09 +0200

    Add a new universal queued task type

---
+++ src/platz1de/EasyEdit/task/queued/QueuedSimpleTask.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace platz1de\EasyEdit\task\queued;
+
+use platz1de\EasyEdit\EasyEdit;
+use Threaded;
+
+class QueuedSimpleTask implements QueuedTask
+{
+	private Threaded $task;
+
+	/**
+	 * QueuedSimpleTask constructor.
+	 * @param Threaded $task
+	 */
+	public function __construct(Threaded $task)
+	{
+		$this->task = $task;
+	}
+
+	public function isInstant(): bool
+	{
+		return false;
+	}
+
+	public function execute(): void
+	{
+		EasyEdit::getWorker()->stack($this->task);
+	}
+
+	public function continue(): bool
+	{
+		return $this->task->isFinished();
+	}
+}
