cd284de5f8d012a76b7b05f727dd8a92e36dc8ae
platz1de <51201131+platz1de@users.noreply.github.com>
2022-03-17 21:38:13 +0100

    Fix formatting

--- src/platz1de/EasyEdit/selection/BlockListSelection.php
+++ src/platz1de/EasyEdit/selection/BlockListSelection.php
@@ -69,12 +69,10 @@ abstract class BlockListSelection extends Selection
 	 */
 	public function merge(BlockListSelection $selection): void
 	{
-		if($selection->getPos1()->getY() < $this->getPos1()->getY())
-        {
-            $this->setPos1($this->getPos1()->withComponents(null, $selection->getPos1()->getY(), null));
-        }
-		if($selection->getPos2()->getY() > $this->getPos2()->getY())
-		{
+		if ($selection->getPos1()->getY() < $this->getPos1()->getY()) {
+			$this->setPos1($this->getPos1()->withComponents(null, $selection->getPos1()->getY(), null));
+		}
+		if ($selection->getPos2()->getY() > $this->getPos2()->getY()) {
 			$this->setPos2($this->getPos2()->withComponents(null, $selection->getPos2()->getY(), null));
 		}
 		foreach ($selection->getTiles() as $tile) {
