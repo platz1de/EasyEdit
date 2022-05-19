ef10514d92f632dc78c7c11fd1f548face4c3797
platz1de <51201131+platz1de@users.noreply.github.com>
2022-05-19 22:16:08 +0200

    Stop wrapping single blocks

--- src/platz1de/EasyEdit/pattern/PatternWrapper.php
+++ src/platz1de/EasyEdit/pattern/PatternWrapper.php
@@ -15,7 +15,7 @@ final class PatternWrapper extends Pattern
 	 */
 	public static function wrap(array $pieces): Pattern
 	{
-		if (count($pieces) === 1 && ($pieces[0] instanceof self || $pieces[0] instanceof PatternConstruct) && $pieces[0]->getWeight() === 100) {
+		if (count($pieces) === 1 && ($pieces[0] instanceof self || $pieces[0] instanceof PatternConstruct || $pieces[0] instanceof SolidBlock) && $pieces[0]->getWeight() === 100) {
 			return $pieces[0]; //no need to wrap single patterns
 		}
 
