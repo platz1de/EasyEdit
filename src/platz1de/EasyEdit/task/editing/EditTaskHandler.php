4eb8840287b93a99ba1cb63a4a4623e321259264
platz1de <51201131+platz1de@users.noreply.github.com>
2022-08-05 15:48:22 +0200

    Properly remove tiles on block overwrite Closes #27

--- src/platz1de/EasyEdit/task/editing/EditTaskHandler.php
+++ src/platz1de/EasyEdit/task/editing/EditTaskHandler.php
@@ -156,6 +156,7 @@ class EditTaskHandler
 		$this->changes->addTile($this->origin->getTile($x, $y, $z));
 
 		$this->result->setBlock($x, $y, $z, $block);
+		$this->result->setTile($x, $y, $z, null);
 	}
 
 	/**
