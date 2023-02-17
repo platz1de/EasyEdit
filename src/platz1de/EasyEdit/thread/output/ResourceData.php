24b1f1a28f6d13c07694dafb6cc6108f352f9d47
platz1de <51201131+platz1de@users.noreply.github.com>
2023-02-17 13:17:40 +0100

    Remove support for custom blocks (this is not possible the same way as previously anymore)

--- src/platz1de/EasyEdit/thread/output/ResourceData.php
+++ src/platz1de/EasyEdit/thread/output/ResourceData.php
@@ -3,7 +3,6 @@
 namespace platz1de\EasyEdit\thread\output;
 
 use platz1de\EasyEdit\convert\BlockStateConvertor;
-use platz1de\EasyEdit\thread\input\RuntimeInputData;
 use platz1de\EasyEdit\utils\ExtendedBinaryStream;
 
 class ResourceData extends OutputData
@@ -17,10 +16,7 @@ class ResourceData extends OutputData
 		$this->rawBTJ = $rawBTJ;
 	}
 
-	public function handle(): void
-	{
-		RuntimeInputData::create();
-	}
+	public function handle(): void {}
 
 	public function putData(ExtendedBinaryStream $stream): void
 	{
