c934dc520abaaa4587643f94b20b5671dfae7266
platz1de <51201131+platz1de@users.noreply.github.com>
2023-05-31 03:40:46 +0200

    Added option to keep benchmarked world after completing

--- src/platz1de/EasyEdit/command/defaults/utility/BenchmarkCommand.php
+++ src/platz1de/EasyEdit/command/defaults/utility/BenchmarkCommand.php
@@ -2,11 +2,11 @@
 
 namespace platz1de\EasyEdit\command\defaults\utility;
 
-use Generator;
-use platz1de\EasyEdit\command\EasyEditCommand;
 use platz1de\EasyEdit\command\flags\CommandFlag;
 use platz1de\EasyEdit\command\flags\CommandFlagCollection;
+use platz1de\EasyEdit\command\flags\SingularCommandFlag;
 use platz1de\EasyEdit\command\KnownPermissions;
+use platz1de\EasyEdit\command\SimpleFlagArgumentCommand;
 use platz1de\EasyEdit\session\Session;
 use platz1de\EasyEdit\session\SessionManager;
 use platz1de\EasyEdit\task\benchmark\BenchmarkManager;
@@ -14,11 +14,11 @@ use platz1de\EasyEdit\utils\MessageComponent;
 use platz1de\EasyEdit\utils\MessageCompound;
 use platz1de\EasyEdit\utils\MixedUtils;
 
-class BenchmarkCommand extends EasyEditCommand
+class BenchmarkCommand extends SimpleFlagArgumentCommand
 {
 	public function __construct()
 	{
-		parent::__construct("/benchmark", [KnownPermissions::PERMISSION_MANAGE]);
+		parent::__construct("/benchmark", [], [KnownPermissions::PERMISSION_MANAGE]);
 	}
 
 	/**
@@ -54,7 +54,7 @@ class BenchmarkCommand extends EasyEditCommand
 				"{time}" => (string) round($time, 2),
 				"{results}" => $resultMsg
 			]);
-		});
+		}, !$flags->hasFlag("keep"));
 	}
 
 	/**
@@ -63,17 +63,8 @@ class BenchmarkCommand extends EasyEditCommand
 	 */
 	public function getKnownFlags(Session $session): array
 	{
-		return [];
-	}
-
-	/**
-	 * @param CommandFlagCollection $flags
-	 * @param Session               $session
-	 * @param string[]              $args
-	 * @return Generator<CommandFlag>
-	 */
-	public function parseArguments(CommandFlagCollection $flags, Session $session, array $args): Generator
-	{
-		yield from [];
+		return [
+			"keep" => new SingularCommandFlag("keep", [], "k") //intentionally not documented, as it provides no benefits other than simplified debugging
+		];
 	}
 }
