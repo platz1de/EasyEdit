f5193b6754558b8346b88e8920a2448e4a9f7bbc
Jonas <jonaswindmann@gmail.com>
2025-08-02 18:27:42 +0200

    Add ellipsoid and pyramid shape support to brushes
    - Introduced `Ellipsoid` and `Pyramid` structures with constructors.
    - Extended BrushHandler with new brush types: Pyramid and Ellipsoid.
    - Added corresponding sub-commands: `EllipsoidBrushSubCommand` and `PyramidBrushSubCommand`.
    - Updated command configurations and handlers to register new brushes.

--- src/platz1de/EasyEdit/brush/BrushHandler.php
+++ src/platz1de/EasyEdit/brush/BrushHandler.php
@@ -7,8 +7,15 @@ use platz1de\EasyEdit\math\OffGridBlockVector;
 use platz1de\EasyEdit\pattern\functional\NaturalizePattern;
 use platz1de\EasyEdit\pattern\parser\ParseError;
 use platz1de\EasyEdit\pattern\parser\PatternParser;
+use platz1de\EasyEdit\selection\Cone;
+use platz1de\EasyEdit\selection\Cube;
 use platz1de\EasyEdit\selection\Cylinder;
+use platz1de\EasyEdit\selection\Ellipsoid;
+use platz1de\EasyEdit\selection\Extrude;
+use platz1de\EasyEdit\selection\Flatten;
+use platz1de\EasyEdit\selection\Pyramid;
 use platz1de\EasyEdit\selection\Sphere;
+use platz1de\EasyEdit\selection\Torus;
 use platz1de\EasyEdit\session\SessionManager;
 use platz1de\EasyEdit\task\editing\DynamicPasteTask;
 use platz1de\EasyEdit\task\editing\SetTask;
@@ -25,6 +32,11 @@ class BrushHandler
 	public const BRUSH_NATURALIZE = 2;
 	public const BRUSH_CYLINDER = 3;
 	public const BRUSH_PASTE = 4;
+	public const BRUSH_CUBE = 5;
+	public const BRUSH_PYRAMID = 6;
+	public const BRUSH_CONE = 7;
+	public const BRUSH_TORUS = 8;
+	public const BRUSH_ELLIPSOID = 9;
 
 	/**
 	 * @param CompoundTag $brush
@@ -57,6 +69,22 @@ class BrushHandler
 					case self::BRUSH_CYLINDER:
 						$session->runSettingTask(new SetTask(new Cylinder($world, $point, $brush->getFloat("brushSize", 0), $brush->getShort("brushHeight", 0)), PatternParser::parseInternal($brush->getString("brushPattern", "stone"))));
 						break;
+					case self::BRUSH_CUBE:
+						$size = $brush->getInt("brushSize", 5);
+						$session->runSettingTask(new SetTask(new Cube($world, $point->add(-$size, -$size, -$size)->forceIntoGrid(), $point->add($size, $size, $size)->forceIntoGrid()), PatternParser::parseInternal($brush->getString("brushPattern", "stone"))));
+						break;
+					case self::BRUSH_PYRAMID:
+						$session->runSettingTask(new SetTask(new Pyramid($world, $point, $brush->getInt("brushSize", 5), $brush->getInt("brushHeight", 10)), PatternParser::parseInternal($brush->getString("brushPattern", "stone"))));
+						break;
+					case self::BRUSH_CONE:
+						$session->runSettingTask(new SetTask(new Cone($world, $point, $brush->getFloat("brushSize", 5.0), $brush->getInt("brushHeight", 10)), PatternParser::parseInternal($brush->getString("brushPattern", "stone"))));
+						break;
+					case self::BRUSH_TORUS:
+						$session->runSettingTask(new SetTask(new Torus($world, $point, $brush->getFloat("majorRadius", 8.0), $brush->getFloat("minorRadius", 3.0)), PatternParser::parseInternal($brush->getString("brushPattern", "stone"))));
+						break;
+					case self::BRUSH_ELLIPSOID:
+						$session->runSettingTask(new SetTask(new Ellipsoid($world, $point, $brush->getFloat("radiusX", 5.0), $brush->getFloat("radiusY", 5.0), $brush->getFloat("radiusZ", 5.0)), PatternParser::parseInternal($brush->getString("brushPattern", "stone"))));
+						break;
 					case self::BRUSH_PASTE:
 						$clipboard = SessionManager::get($player)->getClipboard();
 						if (!$clipboard->isValid()) {
@@ -82,7 +110,12 @@ class BrushHandler
 			"smooth", "smoothing" => self::BRUSH_SMOOTH,
 			"naturalize", "nat", "naturalized" => self::BRUSH_NATURALIZE,
 			"cylinder", "cyl", "cy" => self::BRUSH_CYLINDER,
-			"paste", "pasting" => self::BRUSH_PASTE
+			"paste", "pasting" => self::BRUSH_PASTE,
+			"cube", "cb" => self::BRUSH_CUBE,
+			"pyramid", "pyr" => self::BRUSH_PYRAMID,
+			"cone", "co" => self::BRUSH_CONE,
+			"torus", "ring" => self::BRUSH_TORUS,
+			"ellipsoid", "ell" => self::BRUSH_ELLIPSOID
 		};
 	}
 
@@ -97,7 +130,12 @@ class BrushHandler
 			self::BRUSH_SMOOTH => "smooth",
 			self::BRUSH_NATURALIZE => "naturalize",
 			self::BRUSH_CYLINDER => "cylinder",
-			self::BRUSH_PASTE => "paste"
+			self::BRUSH_PASTE => "paste",
+			self::BRUSH_CUBE => "cube",
+			self::BRUSH_PYRAMID => "pyramid",
+			self::BRUSH_CONE => "cone",
+			self::BRUSH_TORUS => "torus",
+			self::BRUSH_ELLIPSOID => "ellipsoid"
 		};
 	}
 }
---
+++ src/platz1de/EasyEdit/command/defaults/brush/ConeBrushSubCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace platz1de\EasyEdit\command\defaults\brush;
+
+use platz1de\EasyEdit\brush\BrushHandler;
+use platz1de\EasyEdit\command\flags\CommandFlag;
+use platz1de\EasyEdit\command\flags\CommandFlagCollection;
+use platz1de\EasyEdit\command\flags\FloatCommandFlag;
+use platz1de\EasyEdit\command\flags\IntegerCommandFlag;
+use platz1de\EasyEdit\command\flags\SingularCommandFlag;
+use platz1de\EasyEdit\command\flags\StringyPatternCommandFlag;
+use platz1de\EasyEdit\session\Session;
+use pocketmine\nbt\tag\CompoundTag;
+
+class ConeBrushSubCommand extends BrushSubCommand
+{
+	protected const BRUSH_TYPE = BrushHandler::BRUSH_CONE;
+
+	public function __construct()
+	{
+		parent::__construct(["cone", "co"], ["size" => false, "height" => false, "pattern" => false, "gravity" => false]);
+	}
+
+	/**
+	 * @param CompoundTag           $nbt
+	 * @param CommandFlagCollection $flags
+	 */
+	protected function applyBrushNbt(CompoundTag $nbt, CommandFlagCollection $flags): void
+	{
+		$nbt->setFloat("brushSize", $flags->getFloatFlag("size"));
+		$nbt->setInt("brushHeight", $flags->getIntFlag("height"));
+		$nbt->setString("brushPattern", $flags->hasFlag("gravity") ? "gravity(" . $flags->getStringFlag("pattern") . ")" : $flags->getStringFlag("pattern"));
+	}
+
+	/**
+	 * @param Session $session
+	 * @return CommandFlag[]
+	 */
+	public function getKnownFlags(Session $session): array
+	{
+		return [
+			"size" => FloatCommandFlag::default(5.0, "size", ["radius", "rad"], "s"),
+			"pattern" => StringyPatternCommandFlag::default("stone", "pattern", [], "p"),
+			"gravity" => new SingularCommandFlag("gravity", [], "g"),
+			"height" => IntegerCommandFlag::default(10, "height", [], "h")
+		];
+	}
+}
