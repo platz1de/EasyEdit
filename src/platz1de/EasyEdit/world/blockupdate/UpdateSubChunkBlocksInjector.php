68ea404cdf5c7ac9aabfb9782ca129b531f9334f
フロリアン <112857696+floriiian@users.noreply.github.com>
2025-10-07 22:58:18 +0200

    Update UpdateSubChunkBlocksInjector.php
    Refactor packet encoding to use pmmp/ext-encoding

--- src/platz1de/EasyEdit/world/blockupdate/UpdateSubChunkBlocksInjector.php
+++ src/platz1de/EasyEdit/world/blockupdate/UpdateSubChunkBlocksInjector.php
@@ -2,6 +2,8 @@
 
 namespace platz1de\EasyEdit\world\blockupdate;
 
+use pmmp\encoding\ByteBufferReader;
+use pmmp\encoding\ByteBufferWriter;
 use pocketmine\network\mcpe\protocol\ClientboundPacket;
 use pocketmine\network\mcpe\protocol\DataPacket;
 use pocketmine\network\mcpe\protocol\PacketHandlerInterface;
@@ -14,40 +16,35 @@ use pocketmine\network\mcpe\protocol\UpdateSubChunkBlocksPacket;
  *
  * Warning: If the packet syntax changes, this will silently break, not displaying any changes to the client
  */
-class UpdateSubChunkBlocksInjector extends DataPacket implements ClientboundPacket
-{
-	public const NETWORK_ID = ProtocolInfo::UPDATE_SUB_CHUNK_BLOCKS_PACKET;
+class UpdateSubChunkBlocksInjector extends DataPacket implements ClientboundPacket {
+    public const NETWORK_ID = ProtocolInfo::UPDATE_SUB_CHUNK_BLOCKS_PACKET;
 
-	/**
-	 * Binary data to inject
-	 */
-	private string $rawData;
+    /**
+     * Binary data to inject
+     */
+    private string $rawData;
 
-	public static function create(string $data): self
-	{
-		$result = new self;
-		$result->rawData = $data;
-		return $result;
-	}
+    public static function create(string $data): self {
+        $result = new self;
+        $result->rawData = $data;
+        return $result;
+    }
 
-	protected function decodePayload(PacketSerializer $in): void
-	{
-		$prev = $in->getOffset();
-		$mock = new UpdateSubChunkBlocksPacket();
-		$mock->decodePayload($in);
-		$this->rawData = substr($in->getBuffer(), $prev, $in->getOffset() - $prev);
-	}
+    protected function decodePayload(ByteBufferReader $in): void {
+        $prev = $in->getOffset();
+        $mock = new UpdateSubChunkBlocksPacket();
+        $mock->decodePayload($in);
+        $this->rawData = substr($in->getData(), $prev, $in->getOffset() - $prev);
+    }
 
-	protected function encodePayload(PacketSerializer $out): void
-	{
-		$out->put($this->rawData);
-	}
+    protected function encodePayload(ByteBufferWriter $out): void {
+        $out->writeByteArray($this->rawData);
+    }
 
-	public function handle(PacketHandlerInterface $handler): bool
-	{
-		//Apparently some plugins just blindly handle packets sent to the network, so we need to emulate its behavior
-		$mock = new UpdateSubChunkBlocksPacket();
-		$mock->decodePayload(PacketSerializer::decoder($this->rawData, 0));
-		return $mock->handle($handler);
-	}
+    public function handle(PacketHandlerInterface $handler): bool {
+        //Apparently some plugins just blindly handle packets sent to the network, so we need to emulate its behavior
+        $mock = new UpdateSubChunkBlocksPacket();
+        $mock->decodePayload(new ByteBufferReader($this->rawData));
+        return $mock->handle($handler);
+    }
 }
