700eea91ae2f491ee6401dc22d928efd0e7f5618
フロリアン <112857696+floriiian@users.noreply.github.com>
2025-10-08 07:23:39 +0200

    Update UpdateSubChunkBlocksInjector.php
    Refractor curly brackets

--- src/platz1de/EasyEdit/world/blockupdate/UpdateSubChunkBlocksInjector.php
+++ src/platz1de/EasyEdit/world/blockupdate/UpdateSubChunkBlocksInjector.php
@@ -16,7 +16,8 @@ use pocketmine\network\mcpe\protocol\UpdateSubChunkBlocksPacket;
  *
  * Warning: If the packet syntax changes, this will silently break, not displaying any changes to the client
  */
-class UpdateSubChunkBlocksInjector extends DataPacket implements ClientboundPacket {
+class UpdateSubChunkBlocksInjector extends DataPacket implements ClientboundPacket 
+{
     public const NETWORK_ID = ProtocolInfo::UPDATE_SUB_CHUNK_BLOCKS_PACKET;
 
     /**
@@ -24,24 +25,28 @@ class UpdateSubChunkBlocksInjector extends DataPacket implements ClientboundPack
      */
     private string $rawData;
 
-    public static function create(string $data): self {
+    public static function create(string $data): self 
+    {
         $result = new self;
         $result->rawData = $data;
         return $result;
     }
 
-    protected function decodePayload(ByteBufferReader $in): void {
+    protected function decodePayload(ByteBufferReader $in): void 
+    {
         $prev = $in->getOffset();
         $mock = new UpdateSubChunkBlocksPacket();
         $mock->decodePayload($in);
         $this->rawData = substr($in->getData(), $prev, $in->getOffset() - $prev);
     }
 
-    protected function encodePayload(ByteBufferWriter $out): void {
+    protected function encodePayload(ByteBufferWriter $out): void 
+    {
         $out->writeByteArray($this->rawData);
     }
 
-    public function handle(PacketHandlerInterface $handler): bool {
+    public function handle(PacketHandlerInterface $handler): bool 
+    {
         //Apparently some plugins just blindly handle packets sent to the network, so we need to emulate its behavior
         $mock = new UpdateSubChunkBlocksPacket();
         $mock->decodePayload(new ByteBufferReader($this->rawData));
