c7cd37505ee24c5cfb3bd91a2dd9c52005f9333d
kostamax27 <real.kostamax27@gmail.com>
2025-12-02 01:13:25 +0300

    ???

--- src/platz1de/EasyEdit/utils/ArgumentParser.php
+++ src/platz1de/EasyEdit/utils/ArgumentParser.php
@@ -2,6 +2,8 @@
 
 namespace platz1de\EasyEdit\utils;
 
+use platz1de\EasyEdit\command\EasyEditCommand;
+use platz1de\EasyEdit\command\exception\InvalidUsageException;
 use platz1de\EasyEdit\math\OffGridBlockVector;
 use platz1de\EasyEdit\pattern\parser\ParseError;
 use platz1de\EasyEdit\session\Session;
@@ -54,7 +56,7 @@ class ArgumentParser
 	 * @param int|null    $amount
 	 * @return OffGridBlockVector
 	 */
-	public static function parseDirectionVector(Session $session, string $args1 = null, string $args2 = null, ?int &$amount = null) : OffGridBlockVector
+	public static function parseDirectionVector(Session $session, ?string $args1 = null, ?string $args2 = null, ?int &$amount = null): OffGridBlockVector
 	{
 		$amount = 1;
 		if (is_numeric($args1)) {
@@ -74,7 +76,7 @@ class ArgumentParser
 	 * @param string|null $direction
 	 * @return int
 	 */
-	public static function parseFacing(Session $session, ?string $direction = null) : int
+	public static function parseFacing(Session $session, ?string $direction = null): int
 	{
 		return match (strtolower($direction ?? "")) {
 			"north", "n", "-z" => Facing::NORTH,
